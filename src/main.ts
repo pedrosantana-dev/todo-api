840f5601ca8bf32cdaca62af1b0700debfc094dd
Pedro Santana <pedromanu27@gmail.com>
2021-10-16 02:23:04 -0300

    cors habilitado

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+	.setTitle("Nest Todo API")
+	.setDescription("API de Tarefas a fazer")
+	.setVersion("1.0")
+	.build();
+	
   app.setGlobalPrefix('api');
+  app.enableCors({
+	  origin: [ "http://localhost:4200"]
+  });
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
